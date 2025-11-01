52cc05f1743f5e361921578326624d57513565e9
arshitrupanii <arshitrupani6@gmail.com>
2025-11-01 10:29:28 +0530

    update mac

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { HeroUIProvider } from "@heroui/react";
+import { lightTheme, darkTheme } from "../theme.config.js";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <HeroUIProvider
+        defaultTheme="dark"
+        themes={{
+          light: lightTheme,
+          dark: darkTheme,
+        }}
+      >
+        <main className="text-foreground bg-background">
+          <App />
+        </main>
+      </HeroUIProvider>
     </BrowserRouter>
   </StrictMode>
 );
